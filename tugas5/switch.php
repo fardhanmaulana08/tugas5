042d4d2f7744b06fa4192f8e1e1d97c878c50975
Fardhan Maulana Yunandar <fardhanyundr@gmail.com>
2026-01-15 10:30:08 +0700

    Add files via upload

---
+++ tugas5/switch.php
@@ -0,0 +1,16 @@
+<?php
+$hari = 2;
+switch ($hari) {
+    case 1:
+        echo "Senin";
+        break;
+    case 2:
+        echo "Selasa";
+        break;
+    case 3:
+        echo "Rabu";
+        break;
+    default:
+        echo "Hari tidak dikenal";
+}
+?>
